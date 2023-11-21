881f1a5450b4b9ece1f3f4d3cc0a62008d90abc4
joao-mororo <joaovitormororo2003@gmail.com>
2023-11-21 17:07:02 -0300

    v1

--- app/page.js
+++ app/page.js
@@ -1,95 +1,51 @@
-import Image from 'next/image'
+'use client'
+
+import moment from 'moment';
+import { Box, Button, Container } from '@mui/material'
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import { useEffect, useState } from 'react'
 import styles from './page.module.css'
+import { DateCalendar, DatePicker } from '@mui/x-date-pickers';
 
 export default function Home() {
+  const [date, setDate] = useState(moment())
+  const [note, setNote] = useState('')
+  const date_format = 'YYYY-MM-DD'
+
+  useEffect(() => {
+    setNote(localStorage.getItem(moment(date).format(date_format)) || '')
+  }, [date])
+
+  const handleNotes = (text) => {
+    setNote(text)
+    localStorage.setItem(moment(date).format(date_format), text)
+    if (text === '') {
+      localStorage.removeItem(moment(date).format(date_format))
+    }
+  }
+
+  console.log('render');
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <Container maxWidth='lg' sx={{ display: 'flex', justifyContent: 'space-between', padding: '2rem 0' }}>
+        <Box><h1>Agenda Online</h1></Box>
+        <Box>
+        </Box>
+      </Container>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Container maxWidth="lg" sx={{ display: 'flex', height: '80vh' }}>
+        <Box sx={{ width: '70%' }}>
+          <textarea className={styles.notepad} value={note} onChange={(e) => handleNotes(e.target.value)}></textarea>
+        </Box>
+        <Box sx={{ width: '30%', textAlign: 'center' }}>
+          <h1>{moment(date).format('DD/MM/YYYY')}</h1>
+          <DateCalendar value={date} onChange={(newDate) => setDate(newDate)} />
+          <Button variant='contained' onClick={() => setDate(moment())}>Hoje</Button>
+        </Box>
+      </Container>
+    </LocalizationProvider>
   )
 }
