5ce433ab07bb851809acbac09773dd29ad804274
joao-mororo <joaovitormororo2003@gmail.com>
2023-12-07 11:43:04 -0300

    add icon shortcut

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,33 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Agenda Online',
-  description: 'Planeje seus dias com anotações organizadas com um calendário',
-}
+  title: "Agenda Online",
+  description: "Planeje seus dias com anotações organizadas com um calendário",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable
+          pauseOnHover={false}
+          theme="colored"
+        />
+      </body>
     </html>
-  )
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,51 +1,76 @@
-'use client'
+"use client";
 
-import moment from 'moment';
-import { Box, Button, Container } from '@mui/material'
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
-import { useEffect, useState } from 'react'
-import styles from './page.module.css'
-import { DateCalendar, DatePicker } from '@mui/x-date-pickers';
+import moment from "moment";
+import { Box, Button, ButtonGroup, Container } from "@mui/material";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+import { useEffect, useState } from "react";
+import { DateCalendar, DatePicker } from "@mui/x-date-pickers";
+import { toast } from "react-toastify";
+import styles from "./page.module.css";
 
 export default function Home() {
-  const [date, setDate] = useState(moment())
-  const [note, setNote] = useState('')
-  const date_format = 'YYYY-MM-DD'
+  const [date, setDate] = useState(moment());
+  const [note, setNote] = useState("");
+  const formated_date = moment(date).format("YYYY-MM-DD");
+  const icons = ["✅", "❌", "⚠️", "‼️", "❓", "💤", "⏩"];
 
   useEffect(() => {
-    setNote(localStorage.getItem(moment(date).format(date_format)) || '')
-  }, [date])
+    setNote(localStorage.getItem(formated_date) || "");
+  }, [date]);
 
   const handleNotes = (text) => {
-    setNote(text)
-    localStorage.setItem(moment(date).format(date_format), text)
-    if (text === '') {
-      localStorage.removeItem(moment(date).format(date_format))
+    setNote(text);
+    localStorage.setItem(formated_date, text);
+    if (text === "") {
+      localStorage.removeItem(formated_date);
     }
-  }
+  };
 
-  console.log('render');
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text);
+    toast.success(`${text} copiado`);
+  };
 
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
-      <Container maxWidth='lg' sx={{ display: 'flex', justifyContent: 'space-between', padding: '2rem 0' }}>
-        <Box><h1>Agenda Online</h1></Box>
+      <Container
+        maxWidth="lg"
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: "2rem 0",
+        }}
+      >
         <Box>
+          <h1>Agenda Online</h1>
+        </Box>
+        <Box>
+          <ButtonGroup variant="outlined" aria-label="outlined button group">
+            {icons.map((icon) => (
+              <Button onClick={() => copyToClipboard(icon)}>{icon}</Button>
+            ))}
+          </ButtonGroup>
         </Box>
       </Container>
 
-      <Container maxWidth="lg" sx={{ display: 'flex', height: '80vh' }}>
-        <Box sx={{ width: '70%' }}>
-          <textarea className={styles.notepad} value={note} onChange={(e) => handleNotes(e.target.value)}></textarea>
+      <Container maxWidth="lg" sx={{ display: "flex", height: "80vh" }}>
+        <Box sx={{ width: "70%" }}>
+          <textarea
+            className={styles.notepad}
+            value={note}
+            onChange={(e) => handleNotes(e.target.value)}
+          ></textarea>
         </Box>
-        <Box sx={{ width: '30%', textAlign: 'center' }}>
-          <h1>{moment(date).format('DD/MM/YYYY')}</h1>
+        <Box sx={{ width: "30%", textAlign: "center" }}>
+          <h1>{moment(date).format("DD/MM/YYYY")}</h1>
           <DateCalendar value={date} onChange={(newDate) => setDate(newDate)} />
-          <Button variant='contained' onClick={() => setDate(moment())}>Hoje</Button>
+          <Button variant="contained" onClick={() => setDate(moment())}>
+            Hoje
+          </Button>
         </Box>
       </Container>
     </LocalizationProvider>
-  )
+  );
 }
